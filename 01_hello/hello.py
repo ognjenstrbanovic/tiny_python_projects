a42f927b88c6a218f32d2a7e385129bbda2730ce
ognjenstrbanovic <ogistrbanovic@gmail.com>
2020-12-09 15:39:40 -0500

    1.9 completed, optional argument added, all tests passed

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 # Purpose: Say hello
-print('Hello, World!')
-
 import argparse
 
 parser = argparse.ArgumentParser(description='Say hello')
-parser.add_argument('name', help='Name to greet')
+parser.add_argument('-n', '--name', metavar='name', default='World', help='Name to greet')
 args = parser.parse_args()
 print(f'Hello, {args.name}!')
