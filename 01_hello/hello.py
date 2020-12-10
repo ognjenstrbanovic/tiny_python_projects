90ad77b3680654decb7ad831bd4b1333223f20a9
ognjenstrbanovic <ogistrbanovic@gmail.com>
2020-12-09 19:52:04 -0500

    completed 1.11.0

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -2,11 +2,14 @@
 # Purpose: Say hello
 import argparse
 
-def main():
-
+def get_args():
     parser = argparse.ArgumentParser(description='Say hello')
     parser.add_argument('-n', '--name', metavar='name', default='World', help='Name to greet')
-    args = parser.parse_args()
+    return parser.parse_args()
+
+def main():
+
+    args = get_args()
     print(f'Hello, {args.name}!')
 
 
